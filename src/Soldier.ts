1b87d7c7e2147978ed98e559c7fb953f9f380d88
juannarowe <juannarowe@gmail.com>
2026-02-17 14:03:01 +0100

    feat: implement Soldier class and pass tests

--- src/Soldier.ts
+++ src/Soldier.ts
@@ -1,1 +1,18 @@
-export class Soldier {}
+export class Soldier {
+    public health: number;
+    public strength: number;
+
+    constructor(health: number, strength: number) {
+    this.health = health;
+    this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage: number) {
+        this.health -= damage;
+    }
+}
+
